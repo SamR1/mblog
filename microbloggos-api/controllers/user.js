618b1821531730dee1b1a0c5fb659f2f5f2ba045
Sam <sam@samr1.net>
2017-09-29 11:20:02 +0200

    API: profile method updated

--- microbloggos-api/controllers/user.js
+++ microbloggos-api/controllers/user.js
@@ -1,7 +1,18 @@
 var   User       = require('../models/User');
 
+
 var getProfile = function(req, res){
-    res.send("getProfile");
+    if (!req.payload._id) {
+        res.status(401).json({
+            "message" : "UnauthorizedError: private profile"
+        });
+    } else {
+        // Otherwise continue
+        User.findById(req.payload._id)
+            .exec(function(err, user) {
+                res.status(200).json(user);
+            });
+    }
 };
 
-exports.getProfile   = getProfile;
+exports.getProfile = getProfile;
