071f86c90b8a8ff7aad75ff9488b8041d567aff0
Sam <sam@samr1.net>
2017-10-01 00:07:09 +0200

    Client: Profile page updated (fetching data from API)

--- microbloggos-client/src/app/components/user/profile/profile.component.ts
+++ microbloggos-client/src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user = null;
+
+  constructor(private http: Http, private authService: AuthService) { }
 
   ngOnInit() {
+      const apiUrl = 'http://localhost:3000/api/profile';
+      const headers = new Headers();
+      headers.append('Authorization', 'Bearer ' + localStorage.getItem('token'));
+      this.http.get(apiUrl, {headers: headers}).subscribe(
+          res => {
+              this.user = res.json();
+          },
+          err => console.log(err));
   }
 
 }
