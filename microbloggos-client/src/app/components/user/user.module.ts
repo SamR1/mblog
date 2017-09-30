7a4e52709d0746bade7d4ccf1822efcf90f81307
Sam <sam@samr1.net>
2017-10-01 00:11:26 +0200

    Client: link to profile fixed in user panel

--- microbloggos-client/src/app/components/user/user.module.ts
+++ microbloggos-client/src/app/components/user/user.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { UserComponent} from './user/user.component';
 import { LoginComponent} from './login/login.component';
 import { RegisterComponent} from './register/register.component';
@@ -9,7 +11,8 @@ import { ProfileComponent } from './profile/profile.component';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   declarations: [
     UserComponent,
